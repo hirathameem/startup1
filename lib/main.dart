73789ff317c524443ef9e47627b6116ac3bec7e9
Nathira <nathira@onloop.io>
2022-12-12 17:05:57 +0800

    Updating main.dart file to check if versioning changes

--- lib/main.dart
+++ lib/main.dart
@@ -14,10 +14,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Startup Name Generators',
+      title: 'Startup Name Generator',
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Startup Name Generators'),
+          title: const Text('Startup Name Generator'),
         ),
         body: const Center(
           child: RandomWords(),
