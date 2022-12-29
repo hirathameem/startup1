1d455bd0f611ede7040fcbe0813c3c3e91920ba9
Nathira <nathira@onloop.io>
2022-12-29 08:48:34 +0800

    Revert "test4"
    This reverts commit 8dec1440b1879580c2d698cb4b3a3755bea60330.

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@
 // found in the LICENSE file.
 import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 
 void main() {
   runApp(const MyApp());
